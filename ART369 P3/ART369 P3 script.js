d2d4c9abfe0a1be442e94b8b40e9bfa660b62b5e
shoshigb <shoshibieler@ShoshiBsMacBook.wireless.yale.internal>
2015-04-04 19:52:39 -0400

    P3

---
+++ ART369 P3/ART369 P3 script.js
@@ -0,0 +1,36 @@
+
+	<SCRIPT LANGUAGE="JavaScript">
+
+    $(document).ready(function(){
+		$("#about-link").hover(function(){
+    		$("#triangle").css("border-bottom", "90px solid #00E68A");
+    		}, function(){
+    		$("#triangle").css("border-bottom", "90px solid #00C95E");
+		});
+
+		$("#who-link").hover(function(){
+    		$("#triangle").css("border-bottom", "90px solid #00CC99");
+    		}, function(){
+    		$("#triangle").css("border-bottom", "90px solid #00C95E");
+		});
+
+		$("#location-link").hover(function(){
+    		$("#triangle").css("border-bottom", "90px solid #99CC00");
+    		}, function(){
+    		$("#triangle").css("border-bottom", "90px solid #00C95E");
+		});
+
+		$("#sponsors-link").hover(function(){
+    		$("#triangle").css("border-bottom", "90px solid #009933");
+    		}, function(){
+    		$("#triangle").css("border-bottom", "90px solid #00C95E");
+		});
+
+		$("#films-link").hover(function(){
+    		$("#triangle").css("border-bottom", "90px solid #00A37A");
+    		}, function(){
+    		$("#triangle").css("border-bottom", "90px solid #00C95E");
+		});
+	});
+
+    </SCRIPT>
