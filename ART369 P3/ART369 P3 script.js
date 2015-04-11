aa5af5ae00aabbc16d2659ece8daea66d1c604fa
shoshigb <shoshibieler@ShoshiBsMacBook.wireless.yale.internal>
2015-04-11 11:41:35 -0400

    P3 javascript

--- ART369 P3/ART369 P3 script.js
+++ ART369 P3/ART369 P3 script.js
@@ -1,5 +1,22 @@
 
+$(document).ready(function() {
 
+
+    var width = $(window).width();
+    var height = $(window).height();
+
+
+function jqUpdateSize(){
+    // Get the dimensions of the viewport
+    var width = $(window).width();
+    var height = $(window).height();
+
+
+    if (width>800) {
+
+            console.log("width is greater than : border");
+
+            $("#triangle").css("border-bottom", "90px solid #00C95E");
 
                 $("#about-link").hover(function(){
                     $("#triangle").css("border-bottom", "90px solid #339966");
@@ -26,16 +43,49 @@
                     }, function(){
                     $("#triangle").css("border-bottom", "90px solid #00C95E");
                 });
+
+    } else {
+
+                console.log("width is less than 800: no border");
+
+                
+                $("#triangle").css("border-bottom", "40px solid #00C95E");
+
+              
+                $("#about-link").hover(function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                    }, function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                });
+
+                $("#who-link").hover(function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                    }, function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                });
+
+                $("#sponsors-link").hover(function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                    }, function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                });
+
+
+
+                $("#films-link").hover(function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                    }, function(){
+                    $("#triangle").css("border-bottom", "40px solid #00C95E");
+                });
+    }
+};
+$(document).ready(jqUpdateSize);    // When the page first loads
+$(window).resize(jqUpdateSize);     // When the browser changes size
            
 
-
+}); 
         
 
-//   }
-//   else {
-//      console.log('More than 960');
-//   }
-// });
 
 
            
