299b863c772ec22184057797cb68927e808109bc
shoshigb <shoshibieler@ShoshiBsMacBook.wireless.yale.internal>
2015-04-12 23:10:31 -0400

    P3 archive

--- ART369 P3/ART369 P3 script.js
+++ ART369 P3/ART369 P3 script.js
@@ -12,6 +12,47 @@ function jqUpdateSize(){
     var height = $(window).height();
 
 
+    $(function()
+    {
+        var $dropdowns = $('li.past'); // Specifying the element is faster for older browsers
+
+    /**
+     * Mouse events
+     *
+     * @description Mimic hoverIntent plugin by waiting for the mouse to 'settle' within the target before triggering
+     */
+    $dropdowns
+        .on('mouseover', function() // Mouseenter (used with .hover()) does not trigger when user enters from outside document window
+        {
+            var $this = $(this);
+
+            if ($this.prop('hoverTimeout'))
+            {
+                $this.prop('hoverTimeout', clearTimeout($this.prop('hoverTimeout')));
+            }
+
+            $this.prop('hoverIntent', setTimeout(function()
+            {
+                $this.addClass('hover');
+            }, 80));
+        })
+        .on('mouseleave', function()
+        {
+            var $this = $(this);
+
+            if ($this.prop('hoverIntent'))
+            {
+                $this.prop('hoverIntent', clearTimeout($this.prop('hoverIntent')));
+            }
+
+            $this.prop('hoverTimeout', setTimeout(function()
+            {
+                $this.removeClass('hover');
+            }, 80));
+        });
+    });
+
+
     if (width>800) {
 
             console.log("width is greater than : border");
@@ -42,6 +83,23 @@ function jqUpdateSize(){
                     $("#triangle").css("border-bottom", "90px solid #00E68A");
                     }, function(){
                     $("#triangle").css("border-bottom", "90px solid #00C95E");
+                });
+
+
+                // $(".link-about").hover(function() {
+                //     $("body.archive").css("background-color", "#99CC00");
+                //     $("img").css("opacity", "0");
+                //     }, function(){
+                //     $("body.archive").css("background-color", "#EEEEEE");
+                //     $("img").css("opacity", "1");
+                // });
+
+                $(".past").hover(function () {
+                  if ( $( ".appear" ).is( ":hidden" ) ) {
+                    $( ".appear" ).slideDown( "fast" );
+                  } else {
+                    $( ".appear" ).hide();
+                  }
                 });
 
     } else {
